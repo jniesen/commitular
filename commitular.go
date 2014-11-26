37dae72ee6770e016ee65a936d0cfa83c2e55c63
Jonathan Niesen <jon.niesen@gmail.com>
2014-11-26 13:27:15 -0500

    feat(commitular.go): add commands for the different types of change

--- commitular.go
+++ commitular.go
@@ -10,5 +10,56 @@ func main() {
 	app.Name = "commitular"
 	app.Usage = "Make a git commit using the Angular commit message format"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "feat",
+			Usage: "new feature",
+			Action: func(c *cli.Context) {
+			},
+		},
+		{
+			Name:  "fix",
+			Usage: "bug fix",
+			Action: func(c *cli.Context) {
+			},
+		},
+		{
+			Name:  "docs",
+			Usage: "documentation only change",
+			Action: func(c *cli.Context) {
+			},
+		},
+		{
+			Name:  "style",
+			Usage: "style only change that does not affect the meaning of the code (white-space, formatting, missing punctuation, etc)",
+			Action: func(c *cli.Context) {
+			},
+		},
+		{
+			Name:  "refactor",
+			Usage: "change that neither fixes a bug or adds a feature",
+			Action: func(c *cli.Context) {
+			},
+		},
+		{
+			Name:  "perf",
+			Usage: "change that improves performance",
+			Action: func(c *cli.Context) {
+			},
+		},
+		{
+			Name:  "test",
+			Usage: "change that improves test coverage",
+			Action: func(c *cli.Context) {
+			},
+		},
+		{
+			Name:  "chore",
+			Usage: "change the affects build processes, tooling, or libraries",
+			Action: func(c *cli.Context) {
+			},
+		},
+	}
+
 	app.Run(os.Args)
 }
